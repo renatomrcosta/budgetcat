9194a7781abb08e1fd36b542163163b6577338c5
Renato Costa <renato.costa@jimdo.com>
2020-04-01 16:13:29 +0200

    initial setup for new shared project

---
+++ storage/src/main/kotlin/com/xunfos/budgetcat/storage/StorageApplication.kt
@@ -0,0 +1,11 @@
+package com.xunfos.budgetcat.storage
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class StorageApplication
+
+fun main(args: Array<String>) {
+    runApplication<StorageApplication>(*args)
+}
